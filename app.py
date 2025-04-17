42b7eeeec10347215a37904ea424479c509a05c9
Imane the analyst <91439165+Imymab@users.noreply.github.com>
2025-04-17 19:28:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -75,8 +75,11 @@ else:
     existing_data = sheet.get_all_values()
     header_offset = 0 if existing_data and "question" in existing_data[0] else 1
     if not st.session_state.annotations:
-       st.session_state.annotations = existing_data[header_offset:]
-       st.session_state.index = min(len(st.session_state.annotations), len(df))
+      st.session_state.annotations = existing_data[header_offset:]
+      if "index_set" not in st.session_state:
+        st.session_state.index = len(st.session_state.annotations)
+        st.session_state.index_set = True
+
 
 
 
